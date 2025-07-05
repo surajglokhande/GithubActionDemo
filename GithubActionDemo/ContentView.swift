6266e29edcb3383008c71b6145ae1f9307c78de1
surajglokhande <surajglokhande@gmail.com>
2025-07-05 15:39:38 +0530

    Initial Commit

---
+++ GithubActionDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubActionDemo
+//
+//  Created by Suraj Lokhande on 05/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubActionDemo/GithubActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionDemoApp.swift
+//  GithubActionDemo
+//
+//  Created by Suraj Lokhande on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
