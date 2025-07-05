9dae4515841ad3dab0d94a86b6f83938288f60f2
surajglokhande <surajglokhande@gmail.com>
2025-07-05 16:27:01 +0530

    new project setip for github actions

--- GithubActionDemoTests/GithubActionDemoTests.swift
+++ GithubActionDemoTests/GithubActionDemoTests.swift
@@ -23,6 +23,7 @@ final class GithubActionDemoTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertEqual(1 + 1, 2, "Math is broken!")
     }
 
     func testPerformanceExample() throws {
